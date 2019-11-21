d42a05e6d5a787c726c9fb3bec51304acc94b55e
midoks <midoks@163.com>
2019-11-21 16:12:14 +0800

    Update dfs_common.go

--- dfs_common.go
+++ dfs_common.go
@@ -19,7 +19,7 @@ import (
 func dPrint(args ...interface{}) {
 	if Config().Debug {
 		now := time.Now().Format("2006-01-02T15:04:05Z")
-		fmt.Print("[DFS-debug--", now, "]", Config().Host, "] ")
+		fmt.Print("[DFS-debug ", "(", now, ") ", Config().Host, "] ")
 		for i := 0; i < len(args); i++ {
 			fmt.Print(args[i], " ")
 		}
